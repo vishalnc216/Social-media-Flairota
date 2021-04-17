eafe73e95a45cde5bb7b109c42ce3232ecb87848
vishalnc216 <vnc216@gmail.com>
2021-04-17 13:47:03 +0530

    export variable

--- src/Home/Story.js
+++ src/Home/Story.js
@@ -11,38 +11,44 @@ const Story = (props) => {
         
     ] = useDataLayerValue();
     
-    const [storyimage, setstoryimage] = useState("")
-    const [storyuserimage, setstoryuserimage] = useState("")
-    const [storyusername, setstoryusername] = useState("")
+    let [storyimage, setstoryimage] = useState("")
+    let [storyuserimage, setstoryuserimage] = useState("")
+    let [storyusername, setstoryusername] = useState("")
 const [storyid, setstoryid] = useState(props.match.params.storyid)
 // forceUpdate()
-var storyindex = storylist.findIndex(obj => obj.user.id==props.match.params.userid);
-    const [i, seti] = useState(0)
-    useEffect(() => {
-      
-    console.log(props);
-       
-
-       console.log(storylist);
-    }, [])
-    var myfunc=setTimeout(
-       function(){
-       
-         console.log(i)
+var i,j;
+var userindex = storylist.findIndex(obj => obj.user.id==props.match.params.userid);
+const getstories  = ()=>{
+   //  console.log(i)
          // return <Redirect to='/login'  />
          // history.push(`/stories/${storylist[storyindex].user.user.username}/${storylist[storyindex].user.user.id}/${storylist[storyindex].stories[i+1].id}`)
-         for (let i = storyindex; i < storylist.length; i++) {
-             for (let j = 0; j < storylist[i].stories.length; j++) {
-               setstoryimage(storylist[i].stories[j].image)
-               setstoryuserimage(storylist[i].stories[j].user.userimage)
-               setstoryusername(storylist[i].user.user.username)
-             }
-               
-           }
-       
-     
-       },3000
-    )
+         for ( i = storyindex; i < storylist.length; i++) {
+          for ( j = 0; j < storylist[i].stories.length; j++) {
+            setTimeout({
+              function(){
+
+                setstoryimage(storyimage=storylist[i].stories[j].image)
+                setstoryuserimage(storyuserimage=storylist[i].stories[j].user.userimage)
+                setstoryusername(storyusername=storylist[i].user.user.username)
+                // console.log(i + " " +j)
+                console.log(storyimage)
+                console.log(storyusername)
+              }
+              
+            }, 3000)
+          }
+           
+        }
+}
+    useEffect(() => {
+      console.log(props);
+      
+      
+      
+      console.log(storylist);
+      
+    }, [])
+   
     return (
         <div className="bg">
         <div className="Story"> 
